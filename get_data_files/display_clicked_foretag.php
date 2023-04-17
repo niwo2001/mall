fff40f2af3e8fdd0cfea76b3c8099816043ec803
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2023-04-17 22:19:29 +0200

    using avg() funktion in enskilda företag

--- get_data_files/display_clicked_foretag.php
+++ get_data_files/display_clicked_foretag.php
@@ -1,46 +1,154 @@
 <?php
 
-include_once("database/db_connection.php");
-$conn = OpenCon();
-$id = 0;
 if(empty($_GET['id'])){
+    echo "<h2 id='companyNameTitle'>Page Content</h2>";
     // set the id to standard.
-    $sql_firstForetag = "SELECT MIN(ID) as min_id FROM foretag";
-    $result_firstForetag = $conn->query($sql_firstForetag);
-    if($result_firstForetag){
-        $row = mysqli_fetch_assoc($result_firstForetag);
-        $id = $row['min_id'];
-        echo "<a href='?id=".$id."'></a>"; 
-    }
-    else{
-        echo "Error: " . mysqli_error($conn);
-    }
 }
 else{
+    include_once("database/db_connection.php");
+    $conn = OpenCon();
     $id = $_GET['id'];
-}
-
-//Print company name
-$sql_foretagNamn = "SELECT NAMN FROM foretag WHERE ID=$id";
-$result_namn = $conn->query($sql_foretagNamn);
-while($r=$result_namn->fetch_assoc()){
-    echo "<h2>".$r['NAMN']."</h2>";
-}
-
-$sql_yearsFromForetag = "SELECT * FROM betalningstid WHERE FORETAG_ID = $id";
-$result_yearsFromForetag = $conn->query($sql_yearsFromForetag);
-
-$years = 3; //controll how many years are displayed
-$labels_years = array();
-$data_faktisk = array();
-$data_avtalad = array();
-
-while(($row = $result_yearsFromForetag->fetch_assoc()) && ($years > 0)){
-    $years = $years-1;
-    // SAVE THE YEAR
-    $labels_years[] = date('Y', strtotime($row['SKAPAT_DATUM']));
     
-    $betYear_id = $row["ID"];
+    //Print company name
+    $sql_foretagNamn = "SELECT NAMN FROM foretag WHERE ID=$id";
+    $result_namn = $conn->query($sql_foretagNamn);
+    while($r=$result_namn->fetch_assoc()){
+        echo "<h2>".$r['NAMN']."</h2>";
+    }
+
+    $sql_yearsFromForetag = "SELECT * FROM betalningstid WHERE FORETAG_ID = $id";
+    $result_yearsFromForetag = $conn->query($sql_yearsFromForetag);
+
+    $years = 3; //controll how many years are displayed
+    $labels_years = array();
+    $data_faktisk = array();
+    $data_avtalad = array();
+
+    while(($row = $result_yearsFromForetag->fetch_assoc()) && ($years > 0)){
+        $years = $years-1;
+        // SAVE THE YEAR
+        $labels_years[] = date('Y', strtotime($row['SKAPAT_DATUM']));
+        
+        $betYear_id = $row["ID"];
+        // avtalad
+        $sql_betuppgiftav = "SELECT AVG(AVTALAD_BETALTID) as avg_avtalad FROM betalningstiduppgift WHERE BETALNINGSTID_ID = $betYear_id";
+        $result_betuppgiftav = $conn->query($sql_betuppgiftav);
+        $row_avtalad = mysqli_fetch_assoc($result_betuppgiftav);
+        $data_avtalad[] = $row_avtalad['avg_avtalad'];
+        // faktisk
+        $sql_betuppgiftfa = "SELECT AVG(FAKTISK_BETALTID) as avg_faktisk FROM betalningstiduppgift WHERE BETALNINGSTID_ID = $betYear_id";
+        $result_betuppgiftfa = $conn->query($sql_betuppgiftfa);
+        $row_faktisk = mysqli_fetch_assoc($result_betuppgiftfa);
+        $data_faktisk[] = $row_faktisk['avg_faktisk'];
+    }
+    CloseCon($conn);
+    // Save data in a JSON format file
+    $json_faktisk = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk, "data_avtalad" => $data_avtalad));
+    $datafile = fopen("samples/enskiltforetag_sample.txt", "w");
+    fwrite($datafile, $json_faktisk);
+    fclose($datafile);
+}
+
+
+// gammal kod
+/*
+
+
+    <?php
+
+    include_once("database/db_connection.php");
+    $conn = OpenCon();
+    $id = 0;
+    if(empty($_GET['id'])){
+        // set the id to standard.
+        $sql_firstForetag = "SELECT MIN(ID) as min_id FROM foretag";
+        $result_firstForetag = $conn->query($sql_firstForetag);
+        if($result_firstForetag){
+            $row = mysqli_fetch_assoc($result_firstForetag);
+            $id = $row['min_id'];
+            echo "<a href='?id=".$id."'></a>"; 
+        }
+        else{
+            echo "Error: " . mysqli_error($conn);
+        }
+    }
+    else{
+        $id = $_GET['id'];
+    }
+
+    //Print company name
+    $sql_foretagNamn = "SELECT NAMN FROM foretag WHERE ID=$id";
+    $result_namn = $conn->query($sql_foretagNamn);
+    $res = mysqli_fetch_assoc($result_namn);
+    echo "<h2>".$res['NAMN']."</h2>";
+
+    // SET VARIABLES
+    $amount_of_periods = 3;
+
+    // Get dates
+    $periods = array();
+    $todaysDate = date('m-d');
+    $theYear = intval(date('Y'));
+    while ($amount_of_periods > 0){
+        $amount_of_periods--;
+        if($todaysDate > '06-30'){ // om företagen inte rapporterat in för nuvarande året måste vi "backa" ett år
+            $p = ($theYear-1) . "-07-01";// 00:00:00.000000";
+        }
+        else{
+            $p = $theYear . "-07-01";// 00:00:00.000000";
+        }
+        $periods[] = $p;
+        $theYear--;
+    }
+    $labels_years = array();
+    $data_faktisk = array();
+    $data_avtalad = array();
+        var_dump($periods);
+
+    foreach($periods as $p){
+    // SAVE THE YEAR
+    $labels_years[] = substr($p, 0, 4);
+    echo "<h5>Start: ".$p."</h5>";
+
+    // Get the average of avtalad bet.
+    $sql_avtalad = "SELECT AVG(AVTALAD_BETALTID) AS avg_avtalad
+        FROM betalningstiduppgift
+        INNER JOIN betalningstid ON betalningstiduppgift.BETALNINGSTID_ID = betalningstid.ID
+        WHERE betalningstid.FORETAG_ID = $id
+        AND betalningstid.SKAPAT_DATUM = $p";
+    $result_avtalad = $conn->query($sql_avtalad);   
+    if(!$result_avtalad){
+        echo "Error: " . mysqli_error($conn);
+    }else{
+        $row_avtalad = mysqli_fetch_assoc($result_avtalad);
+        var_dump($row_avtalad);
+        $data_avtalad[] = $row_avtalad['avg_avtalad'];
+    }
+    // Get the average of avtalad bet.
+    $sql_faktisk = "SELECT AVG(FAKTISK_BETALTID) AS avg_faktisk
+        FROM betalningstiduppgift
+        INNER JOIN betalningstid ON betalningstiduppgift.BETALNINGSTID_ID = betalningstid.ID
+        WHERE betalningstid.FORETAG_ID = $id
+        AND betalningstid.SKAPAT_DATUM = '2021-07-01 00:00:00.000000'";
+    $result_faktisk = $conn->query($sql_faktisk);   
+    if(!$result_faktisk){
+        echo "Error: " . mysqli_error($conn);
+    }else{
+        $row_faktisk = mysqli_fetch_assoc($result_faktisk);
+        var_dump($row_faktisk);
+        $data_faktisk[] = $row_faktisk['avg_faktisk'];
+    }
+    /*
+    // Get the average of faktisk bet.
+    $sql_faktisk = "SELECT AVG(FAKTISK_BETALTID) as avg_faktisk FROM betalningstiduppgift WHERE id = $id AND skapat_datum = '$p'";
+    $result_faktisk = $conn->query($sql_faktisk);   
+    if(!$result_faktisk){
+        echo "Error: " . mysqli_error($conn);
+    }else{
+        $row_faktisk = mysqli_fetch_assoc($result_faktisk);  
+        $data_faktisk[] = $row_faktisk['avg_faktisk'];  
+    }
+    
     $sql_betuppgift = "SELECT * FROM betalningstiduppgift WHERE BETALNINGSTID_ID = $betYear_id";
     $result_betuppgift = $conn->query($sql_betuppgift);
 
@@ -55,81 +163,23 @@ while(($row = $result_yearsFromForetag->fetch_assoc()) && ($years > 0)){
     // Get the average and save data
     $data_faktisk[] = floor($tot_faktisk * (1/3));
     $data_avtalad[] = floor($tot_avtalad * (1/3));
-}
-CloseCon($conn);
-// Save data in a JSON format file
-$json_faktisk = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk, "data_avtalad" => $data_avtalad));
-$datafile = fopen("samples/enskiltforetag_sample.txt", "w");
-fwrite($datafile, $json_faktisk);
-fclose($datafile);
-
-
-
-
-
-/*
-
-    <?php
-
-    if(empty($_GET['id'])){
-        echo "<h2 id='companyNameTitle'>Page Content</h2>";
-        // set the id to standard.
     }
-    else{
-        include_once("database/db_connection.php");
-        $conn = OpenCon();
-        $id = $_GET['id'];
-        
-        //Print company name
-        $sql_foretagNamn = "SELECT NAMN FROM foretag WHERE ID=$id";
-        $result_namn = $conn->query($sql_foretagNamn);
-        while($r=$result_namn->fetch_assoc()){
-            echo "<h2>".$r['NAMN']."</h2>";
-        }
-
-        $sql_yearsFromForetag = "SELECT * FROM betalningstid WHERE FORETAG_ID = $id";
-        $result_yearsFromForetag = $conn->query($sql_yearsFromForetag);
-
-        $years = 3; //controll how many years are displayed
-        $labels_years = array();
-        $data_faktisk = array();
-        $data_avtalad = array();
-
-        while(($row = $result_yearsFromForetag->fetch_assoc()) && ($years > 0)){
-            $years = $years-1;
-            // SAVE THE YEAR
-            $labels_years[] = date('Y', strtotime($row['SKAPAT_DATUM']));
-            
-            $betYear_id = $row["ID"];
-            $sql_betuppgift = "SELECT * FROM betalningstiduppgift WHERE BETALNINGSTID_ID = $betYear_id";
-            $result_betuppgift = $conn->query($sql_betuppgift);
-
-            $tot_faktisk = 0;
-            $tot_avtalad = 0;
-
-            // Get bet.tider for every year
-            while($row2 = $result_betuppgift->fetch_assoc()){
-                $tot_faktisk = $tot_faktisk + $row2["FAKTISK_BETALTID"];
-                $tot_avtalad = $tot_avtalad + $row2["AVTALAD_BETALTID"];
-            }
-            // Get the average and save data
-            $data_faktisk[] = floor($tot_faktisk * (1/3));
-            $data_avtalad[] = floor($tot_avtalad * (1/3));
-        }
-        CloseCon($conn);
-        // Save data in a JSON format file
-        $json_faktisk = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk, "data_avtalad" => $data_avtalad));
-        $datafile = fopen("samples/enskiltforetag_sample.txt", "w");
-        fwrite($datafile, $json_faktisk);
-        fclose($datafile);
-    }
-
-
-    ?>
+    // reverse the arrays
+    $labels_years = array_reverse($labels_years);
+    $data_faktisk = array_reverse($data_faktisk);
+    $data_avtalad = array_reverse($data_avtalad);
+    // Save data in a JSON format file
+    $json_faktisk = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk, "data_avtalad" => $data_avtalad));
+    $datafile = fopen("samples/enskiltforetag_sample.txt", "w");
+    fwrite($datafile, $json_faktisk);
+    fclose($datafile);
 
 
 
+    CloseCon($conn);
+   
 */
+
 
 
 
--- sammanlagd_statistik.php
+++ sammanlagd_statistik.php
@@ -5,11 +5,15 @@
 
 <div class="infoWrapper">
 
-    <h2>Sammanlagd statistik</h2><br>
-    <div id='tot_box'>
-        <h3>Genomsnittlig betalningstid för alla företag</h3>
+    <div class='box' id='tot_box'>
+        <h3>Sammanlagd statistik</h3><br>
         <?php include("get_data_files/tot_data.php") ?>
         <canvas id='tot_chart' width='300' height='250'></canvas>
+        
+    </div>
+    
+    <div id='ranking'>
+        <h3>Top tre företag som betalar fakturor mest i tid!</h3>
         
     </div>
 
