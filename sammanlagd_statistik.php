481793b6818caca0215c383978881c46b2c1fd9e
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2023-04-19 17:44:26 +0200

    Display the percentage of forsenade rakning in tot

--- sammanlagd_statistik.php
+++ sammanlagd_statistik.php
@@ -8,21 +8,27 @@
     <div class='box' id='tot_box'>
         <h3>Sammanlagd statistik</h3><br>
         <?php include("get_data_files/tot_data.php") ?>
-        <canvas id='tot_chart' width='300' height='250'></canvas>
+        <canvas id='tot_barChart' width='300' height='250'></canvas>
         
+    </div>
+
+    <div class='pie_box'>
+        <h3>Procenten av sveriges räkningar från storaföretag till mindre företag</h3>
+        <canvas id='tot_pieChart' style="height:40px; width:80px"></canvas>
     </div>
     
     <div id='ranking'>
         <h3>Top tre företag som betalar fakturor mest i tid!</h3>
         <?php include("get_data_files/list_top.php") ?>
-        <script src="js/printPieChart.js"></script>
-        <script src="js/printBarChart.js"></script>
-        <script>
-            printBarChart('tot_chart', 'samples/Tot_sample.txt', '#176CA1', '#EA7369' );
-        </script>
+        
     </div>
 
-    
+    <script src="js/printBarChart.js"></script>
+    <script src="js/printPieChart.js"></script>
+    <script>
+        printBarChart('tot_barChart', 'samples/Tot_sample.txt', '#176CA1', '#EA7369' );
+        printPieChart('tot_pieChart', 'samples/Tot_andel_sample.txt');
+    </script>
 
 </div>
 
