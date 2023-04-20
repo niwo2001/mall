917a90c195909e456dcea417eb95d315d6ee6a00
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2023-04-20 10:16:30 +0200

    Added canvas for category of enskiltforetag

--- enskilt_foretag.php
+++ enskilt_foretag.php
@@ -18,11 +18,19 @@
             <canvas id='foretag_bar' width='300' height='250'></canvas>
             <canvas id='foretag_pie' style="height:40px; width:80px"></canvas>
         </div>
+        <div class='enskiltforetag_kategorier'>
+            <canvas id='foretag_micro' style="height:20px; width:40px"></canvas>
+            <canvas id='foretag_sma' style="height:20px; width:40px"></canvas>
+            <canvas id='foretag_medel' style="height:20px; width:40px"></canvas>
+        </div>
+
         <script src="js/printBarChart.js"></script>
         <script src="js/printPieChart.js"></script>
         <script>
             printBarChart('foretag_bar', 'samples/enskiltforetag_sample.txt', '#176CA1', '#EA7369' );
             printPieChart('foretag_pie', 'samples/enskiltforetag_andel_sample.txt');
+            
+            //kategorier
         </script>
     </div>
 
