49edb57fdda086742763169619f84366a6c51492
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2023-04-24 12:43:00 +0200

    changed from hem to start in menu

--- get_data_files/display_clicked_foretag.php
+++ get_data_files/display_clicked_foretag.php
@@ -47,7 +47,7 @@ $name = '';
 $data_faktisk = array();
 $data_avtalad = array();
 $data_andel = array();
-// cat
+// kategorier
 $data_faktisk_MI = array();
 $data_avtalad_MI = array();
 $data_faktisk_SM = array();
@@ -90,7 +90,6 @@ foreach($periods as $p){
         $row = mysqli_fetch_assoc($result_cat_mi);
         $data_avtalad_MI[] = $row['AVTALAD'];
         $data_faktisk_MI[] = $row['FAKTISK'];
-        //$data_andel[] = $row['ANDELAR'];
     }else{
         echo "Error: " . mysqli_error($conn);
     } //SMÅ
@@ -106,7 +105,6 @@ foreach($periods as $p){
         $row = mysqli_fetch_assoc($result_cat_sm);
         $data_avtalad_SM[] = $row['AVTALAD'];
         $data_faktisk_SM[] = $row['FAKTISK'];
-        //$data_andel[] = $row['ANDELAR'];
     }else{
         echo "Error: " . mysqli_error($conn);
     } // MEDEL
@@ -122,7 +120,6 @@ foreach($periods as $p){
         $row = mysqli_fetch_assoc($result_cat_me);
         $data_avtalad_ME[] = $row['AVTALAD'];
         $data_faktisk_ME[] = $row['FAKTISK'];
-        //$data_andel[] = $row['ANDELAR'];
     }else{
         echo "Error: " . mysqli_error($conn);
     }
@@ -133,15 +130,17 @@ foreach($periods as $p){
 echo "<h2>".$name."</h2>";
 
 // Save andelar data 
-$json_andelar = json_encode(array('andel_sen' => $data_andel[2], 'andel_ejsen' => (100-$data_andel[2])));
+$json_andelar = json_encode(array('andel_sen' => $data_andel[2], 'andel_ejsen' => $data_andel[2]));
 $datafile_andel = fopen('samples/enskiltforetag_andel_sample.txt', 'w');
 fwrite($datafile_andel, $json_andelar);
 fclose($datafile_andel);
+
 // Save tot data in a JSON format file
 $json_faktisk = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk, "data_avtalad" => $data_avtalad));
 $datafile = fopen("samples/enskiltforetag_sample.txt", "w");
 fwrite($datafile, $json_faktisk);
 fclose($datafile);
+
 // Save cat data in a JSON format file MICROFÖRETAG
 $json_faktisk_MI = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk_MI, "data_avtalad" => $data_avtalad_MI));
 $datafile_MI = fopen("samples/enskiltforetag_cat_mi_sample.txt", "w");
@@ -159,142 +158,4 @@ fwrite($datafile_ME, $json_faktisk_ME);
 fclose($datafile_ME);
 
 CloseCon($conn);
-
-
-
-
-// gammal kod
-/*
-
-
-    <?php
-
-    include_once("database/db_connection.php");
-    $conn = OpenCon();
-    $id = 0;
-    if(empty($_GET['id'])){
-        // set the id to standard.
-        $sql_firstForetag = "SELECT MIN(ID) as min_id FROM foretag";
-        $result_firstForetag = $conn->query($sql_firstForetag);
-        if($result_firstForetag){
-            $row = mysqli_fetch_assoc($result_firstForetag);
-            $id = $row['min_id'];
-            echo "<a href='?id=".$id."'></a>"; 
-        }
-        else{
-            echo "Error: " . mysqli_error($conn);
-        }
-    }
-    else{
-        $id = $_GET['id'];
-    }
-
-    //Print company name
-    $sql_foretagNamn = "SELECT NAMN FROM foretag WHERE ID=$id";
-    $result_namn = $conn->query($sql_foretagNamn);
-    $res = mysqli_fetch_assoc($result_namn);
-    echo "<h2>".$res['NAMN']."</h2>";
-
-    // SET VARIABLES
-    $amount_of_periods = 3;
-
-    // Get dates
-    $periods = array();
-    $todaysDate = date('m-d');
-    $theYear = intval(date('Y'));
-    while ($amount_of_periods > 0){
-        $amount_of_periods--;
-        if($todaysDate > '06-30'){ // om företagen inte rapporterat in för nuvarande året måste vi "backa" ett år
-            $p = ($theYear-1) . "-07-01";// 00:00:00.000000";
-        }
-        else{
-            $p = $theYear . "-07-01";// 00:00:00.000000";
-        }
-        $periods[] = $p;
-        $theYear--;
-    }
-    $labels_years = array();
-    $data_faktisk = array();
-    $data_avtalad = array();
-        var_dump($periods);
-
-    foreach($periods as $p){
-    // SAVE THE YEAR
-    $labels_years[] = substr($p, 0, 4);
-    echo "<h5>Start: ".$p."</h5>";
-
-    // Get the average of avtalad bet.
-    $sql_avtalad = "SELECT AVG(AVTALAD_BETALTID) AS avg_avtalad
-        FROM betalningstiduppgift
-        INNER JOIN betalningstid ON betalningstiduppgift.BETALNINGSTID_ID = betalningstid.ID
-        WHERE betalningstid.FORETAG_ID = $id
-        AND betalningstid.SKAPAT_DATUM = $p";
-    $result_avtalad = $conn->query($sql_avtalad);   
-    if(!$result_avtalad){
-        echo "Error: " . mysqli_error($conn);
-    }else{
-        $row_avtalad = mysqli_fetch_assoc($result_avtalad);
-        var_dump($row_avtalad);
-        $data_avtalad[] = $row_avtalad['avg_avtalad'];
-    }
-    // Get the average of avtalad bet.
-    $sql_faktisk = "SELECT AVG(FAKTISK_BETALTID) AS avg_faktisk
-        FROM betalningstiduppgift
-        INNER JOIN betalningstid ON betalningstiduppgift.BETALNINGSTID_ID = betalningstid.ID
-        WHERE betalningstid.FORETAG_ID = $id
-        AND betalningstid.SKAPAT_DATUM = '2021-07-01 00:00:00.000000'";
-    $result_faktisk = $conn->query($sql_faktisk);   
-    if(!$result_faktisk){
-        echo "Error: " . mysqli_error($conn);
-    }else{
-        $row_faktisk = mysqli_fetch_assoc($result_faktisk);
-        var_dump($row_faktisk);
-        $data_faktisk[] = $row_faktisk['avg_faktisk'];
-    }
-    /*
-    // Get the average of faktisk bet.
-    $sql_faktisk = "SELECT AVG(FAKTISK_BETALTID) as avg_faktisk FROM betalningstiduppgift WHERE id = $id AND skapat_datum = '$p'";
-    $result_faktisk = $conn->query($sql_faktisk);   
-    if(!$result_faktisk){
-        echo "Error: " . mysqli_error($conn);
-    }else{
-        $row_faktisk = mysqli_fetch_assoc($result_faktisk);  
-        $data_faktisk[] = $row_faktisk['avg_faktisk'];  
-    }
-    
-    $sql_betuppgift = "SELECT * FROM betalningstiduppgift WHERE BETALNINGSTID_ID = $betYear_id";
-    $result_betuppgift = $conn->query($sql_betuppgift);
-
-    $tot_faktisk = 0;
-    $tot_avtalad = 0;
-
-    // Get bet.tider for every year
-    while($row2 = $result_betuppgift->fetch_assoc()){
-        $tot_faktisk = $tot_faktisk + $row2["FAKTISK_BETALTID"];
-        $tot_avtalad = $tot_avtalad + $row2["AVTALAD_BETALTID"];
-    }
-    // Get the average and save data
-    $data_faktisk[] = floor($tot_faktisk * (1/3));
-    $data_avtalad[] = floor($tot_avtalad * (1/3));
-    }
-    // reverse the arrays
-    $labels_years = array_reverse($labels_years);
-    $data_faktisk = array_reverse($data_faktisk);
-    $data_avtalad = array_reverse($data_avtalad);
-    // Save data in a JSON format file
-    $json_faktisk = json_encode(array("labels" => $labels_years, "data_faktisk" => $data_faktisk, "data_avtalad" => $data_avtalad));
-    $datafile = fopen("samples/enskiltforetag_sample.txt", "w");
-    fwrite($datafile, $json_faktisk);
-    fclose($datafile);
-
-
-
-    CloseCon($conn);
-   
-*/
-
-
-
-
-?>
 
--- includes/mainmeny.php
+++ includes/mainmeny.php
@@ -1,13 +1,12 @@
 
 <div class="topnav">
-        <a id="b_hem" href="index.php">Hem</a> 
+        <a id="b_hem" href="index.php">Start</a> 
         <a id="b_kategori" href="storlekskategori.php">Storlekskategori</a> 
         <a id="b_foretag" href="enskilt_foretag.php">Enskilt företag</a> 
         <a id="b_sammanlagd" href="sammanlagd_statistik.php">Sammanlagd statistik</a>
 </div>
 
 <script>
-
 if (window.location.pathname === '/index.php') {
     document.getElementById("b_hem").style.backgroundColor ="rgb(0, 74, 173)"
 }
@@ -20,75 +19,4 @@ else if (window.location.pathname === '/enskilt_foretag.php'){
 else if (window.location.pathname === '/sammanlagd_statistik.php'){
     document.getElementById("b_sammanlagd").style.backgroundColor ="rgb(0, 74, 173)"
 }
-
 </script>
-
-<!--
-
-<div id="mainmenu">
-    <button class="buttonClick" id="b_hem"          onclick="location.href='index.php'">Hem</button>
-    <button class="buttonClick" id="b_foretag"      onclick="location.href='enskilt_foretag.php'">Sök enskilt företag</button>
-    <button class="buttonClick" id="b_sammanlagd"   onclick="changeColor('b_sammanlagd'); location.href='sammanlags_statistik.php'">Sammanlagd statistik</button>
-    <button class="buttonClick" id="b_kategori"     onclick="changeColor('b_kategori'); location.href='storlekskategori.php'">Storlekskategori</button>
-</div>
-
-
-
-<script>    
-    
-    const element1 = document.getElementById("b_hem");
-    element1.addEventListener("click", function(){
-        document.getElementById("b_hem").style.backgroundColor ="rgb(17, 115, 251)"
-        document.getElementById("b_foretag").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_sammanlagd").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_kategori").style.backgroundColor ="rgb(0, 74, 173)"
-    })
-
-    const element2 = document.getElementById("b_foretag");
-    element2.addEventListener("click", function(){
-        document.getElementById("b_hem").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_foretag").style.backgroundColor ="rgb(17, 115, 251)"
-        document.getElementById("b_sammanlagd").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_kategori").style.backgroundColor ="rgb(0, 74, 173)"
-    })
-
-    const element3 = document.getElementById("b_sammanlagd");
-    element3.addEventListener("click", function(){
-        document.getElementById("b_hem").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_foretag").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_sammanlagd").style.backgroundColor ="rgb(17, 115, 251)"
-        document.getElementById("b_kategori").style.backgroundColor ="rgb(0, 74, 173)"
-    })
-
-    const element4 = document.getElementById("b_kategori");
-    element4.addEventListener("click", function(){
-        document.getElementById("b_hem").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_foretag").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_sammanlagd").style.backgroundColor ="rgb(0, 74, 173)"
-        document.getElementById("b_kategori").style.backgroundColor ="rgb(17, 115, 251)"
-    })
-
-
-
-
-    function changeColor(buttonId) {
-        // Get all buttons with the class "menuButton"
-        var buttons = document.getElementsByClassName("buttonClick");
-
-        // Remove the "active" class from all buttons
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].classList.remove("active");
-            buttons[i].classList.style.backgroundColor = "#000000"
-        }
-
-        // Add the "active" class to the clicked button
-        var button = document.getElementById(buttonId);
-        button.classList.add("active");
-
-        document.getElementById(buttonId).style.backgroundColor = "rgba(255,0,0,0.8)";
-    }
-
-
-</script>
-
--->
